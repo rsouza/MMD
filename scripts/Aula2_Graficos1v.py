28bde03aa29c479242a16763c974d64c31c4f7a0
Renato Rocha Souza <rsouza.fgv@gmail.com>
2012-06-20 13:52:14 -0300

    Some corrections and adding new datasets

--- scripts/Aula2_Graficos1v.py
+++ scripts/Aula2_Graficos1v.py
@@ -1,50 +1,61 @@
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
 Mestrado em Modelagem Matematica da Informacao
-Disciplina de Modelagem e Mineracao de Dados
+Master Program - Mathematical Modeling of Information
+
+Disciplina: Modelagem e Mineracao de Dados
+Course: Data Mining and Modeling
+
 Professor: Renato Rocha Souza
-Topico: Simple Univariate Graphs (aula2)
-"""
+Topic: Simple Univariate Graphs (class #2)
 
-#Importing the packages that will be used
-#http://docs.scipy.org/doc/
-#http://numpy.scipy.org/
-#http://scipy.org/
-#http://matplotlib.sourceforge.net/
+Information on the Python Packages used:
+http://docs.scipy.org/doc/
+http://numpy.scipy.org/
+http://scipy.org/
+http://matplotlib.sourceforge.net/
+'''
 
-from numpy import loadtxt, arange
-from matplotlib.pyplot import plot, scatter, boxplot, show, title, legend, figure
-from scipy import linspace, polyval, polyfit, sqrt, stats, randn
+import numpy as np
+from numpy import loadtxt, genfromtxt, array, arange
+from matplotlib.pyplot import plot, boxplot, show, title, legend, figure
 from scipy.stats import describe, cumfreq
 from scipy.stats.kde import gaussian_kde
 from pylab import hist
 
-#Specifying the path to the files
+'''Specifying the path to the files'''
 datapath = "/home/rsouza/Documentos/Git/MMD/datasets/"
 dataset1 = "ch02_presidents"
 dataset2 = "ch02_serverdata"
 dataset3 = "ch02_glass.data"
 
-
+'''Some information on the datasets'''
 def bstats(vec):
     s = {'num_element':len(vec), 'minimo':vec.min(),
          'maximo':vec.max(), 'media':vec.mean(),
          'desvio_padrao':vec.std()}
     return s
 
-#Jitter Plots
-#http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.plot
+'''Jitter Plots
+http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.plot'''
 def jitterplots(vec):
     plot(vec)
+    title('default settings')
+    legend('data')
     figure(2)
     plot(vec,'r')
+    title('default + red')    
     figure(3)
     plot(vec,'bo')
+    title('blue and circles')
     figure(4)
     plot(vec,'g.')
+    title('green and dots')
     show()
     
-#Histograms
-#http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.hist
+'''Histograms
+http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.hist'''
 def histo(vec, nbins=100):
     hist(vec, bins=nbins)
     figure(2)
@@ -53,8 +64,8 @@ def histo(vec, nbins=100):
     hist(vec, bins=100*nbins)
     show()
 
-#Kernel Density Estimates
-#http://www.scipy.org/doc/api_docs/SciPy.stats.kde.gaussian_kde.html
+'''Kernel Density Estimates
+http://www.scipy.org/doc/api_docs/SciPy.stats.kde.gaussian_kde.html'''
 def kerndens(vec,nbins=100):
     hist(vec, bins=nbins, normed=True, align='mid')
     figure(2)    
@@ -63,8 +74,8 @@ def kerndens(vec,nbins=100):
          gkde.evaluate(arange(0,(1.01*(max(vec)-min(vec))),.1)))   
     show()
 
-#Cumulative Frequency
-#http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.cumfreq.html#scipy.stats.cumfreq
+'''Cumulative Frequency
+http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.cumfreq.html#scipy.stats.cumfreq'''
 def cumdist(vec, nbins=100):
     hist(vec, bins=nbins, normed=False, align='mid')
     figure(2)    
@@ -72,8 +83,8 @@ def cumdist(vec, nbins=100):
     plot(disc[0]/len(vec)) 
     show()
 
-#Box and Whisker Plots
-#http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.boxplot
+'''Box and Whisker Plots
+http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.boxplot'''
 def bp(vec):
     boxplot(vec, notch=0, sym='+', vert=0, whis=1.5,
             positions=None, widths=None)
@@ -81,36 +92,46 @@ def bp(vec):
 
 
 if __name__ == '__main__':
-    #loading the datasets
-    #http://docs.scipy.org/doc/numpy/reference/generated/numpy.loadtxt.html
-    x = loadtxt(datapath+dataset1, usecols=(2,))
-    y = loadtxt(datapath+dataset2)
-    z = loadtxt(datapath+dataset3, usecols=(1,10), delimiter=',')
+    '''loading the datasets
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.genfromtxt.html'''
+    #x = genfromtxt(datapath+dataset1, usecols=(1,2), dtype=(str,int))
+    x = genfromtxt(datapath+dataset1, usecols=(2))
+    x_names = genfromtxt(datapath+dataset1, usecols=(1), dtype=(str))
+    #x = genfromtxt(datapath+dataset1, usecols=(1,2), names = ['presidents','months'],dtype=[('names','|S12'),('months','i8')])
+    #x = np.array([[t[0] for t in x],[t[1] for t in x]]).T
     
-    #http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.describe.html
-    print(describe(x))
+    '''http://docs.scipy.org/doc/numpy/reference/generated/numpy.loadtxt.html'''
+    y = loadtxt(datapath+dataset2)
+    z = loadtxt(datapath+dataset3, usecols=(1,2,10), delimiter=',')
+    
+    '''http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.describe.html'''
+    #print(describe(x[:,1]))
     print(describe(y))
     print(describe(z))
     print
     
-    #Customized version: bstats
-    #print(bstats(x))
+    '''Customized version: bstats'''
+    print(bstats(x))
+    print(bstats(y))
+    print(bstats(z[:,1]))
 
-    #Examining with Jitter Plots
+    '''Examining with Jitter Plots'''
     #jitterplots(x)
 
-    #Examining with Histograms
-    #Try to change the default bins value
-    #histo(x)
+    '''Examining with Histograms
+    Try to change the default bins value'''
+    #histo(x, 10)
+    #histo(y, 10)
 
-    #Kernel Density Estimates
-    #http://www.scipy.org/doc/api_docs/SciPy.stats.kde.gaussian_kde.html
-    #kerndens(x)
+    '''Kernel Density Estimates
+    http://www.scipy.org/doc/api_docs/SciPy.stats.kde.gaussian_kde.html'''
+    #kerndens(y)
 
-    #Cumulative Frequency
-    #http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.cumfreq.html#scipy.stats.cumfreq
+    '''Cumulative Frequency
+    http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.cumfreq.html#scipy.stats.cumfreq'''
     #cumdist(x)
     
-    #Box and Whisker Plots
-    #http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.boxplot
+    '''Box and Whisker Plots
+    http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.boxplot'''
     #bp(x)
+    #bp(z[:,0])
